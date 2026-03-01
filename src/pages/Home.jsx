10ecf92e74274e7e93acdf689e13aaa7ed69ce15
k-ghale <kabinghale888@gmail.com>
2026-02-28 22:13:09 -0500

    home page started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,30 @@
-// to-do
+//import navbar
+
+const Home = () => {
+  return (
+
+    <>
+    //nav
+      <div className="w-full h-screen  flex items-center justify-center bg-black">
+        <div className="w-6/10 h-6/10 bg-gray-500 rounded">
+          <p className="bg-[#24283b] rounded-t">terminal - home</p>
+          <div className="w-full h- bg-[#1a1b26] rounded-b">
+            <p className="text-[#4c90e9]">user @portfolio</p>
+
+            <p>Name : Kabin Ghale</p>
+            <p>Role : Backend Dev</p>
+            <p>Location : Toronto, Canada</p>
+            <p>Email : kabinghale1@gmail.com </p>
+            <p>Uptime : 1 year in development</p>
+
+            <p>Shell: Javascript</p>
+            <p>DE: React, NextJS, Vue.js</p>
+            <p>Theme: Tokyo Night</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home;
